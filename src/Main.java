b17df5c1fd2d08a79d7a8e018d541e3174b4a415
Meder <meder@MacBook-Pro-meder.local>
2024-02-22 13:00:38 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,43 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    {
+        public static void main(String[] args) {
+        List r = new ArrayList<>();
+        r.add(6.3);
+        r.add(5.9);
+        FavoriteClasses a = new FavoriteClasses("Hello", 67, r.get(0));
+        System.out.println("My favorites are " + a.getFav1() + ", " + a.getFav2() + ", and " + a.getFav3() + ".");
+    }
+
+        public static class FavoriteClasses {
+            private favorite1;
+            private favorite2;
+            private favorite3;
+
+            FavoriteClasses(fav1, fav2, fav3) {
+                this.favorite1 = fav1;
+                this.favorite2 = fav2;
+                this.favorite3 = fav3;
+            }
+
+
+            public getFav1() {
+                return (this.favorite1);
+            }
+
+
+            public getFav2() {
+                return (this.favorite2);
+            }
+
+
+            public getFav3() {
+                return (this.favorite3);
+            }
+
+
+        }
+    }
+}
